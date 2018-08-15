0056ee69970de4e40acfb6c83a231fa464a3d078
Gregory Norwood <Gregoryn@summitfinances.com>
2018-08-15 13:51:44 -0400

    initial commit of full repository

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="card">
+
+        <div class="card-header">
+            Categories
+        </div>
+
+        <div class="card-body">
+
+            <table class="table table-hover">
+                <thead>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Edit
+                </th>
+                <th>
+                    Delete
+                </th>
+                </thead>
+
+                <tbody>
+                    @if($categories->count()>0)
+                             @foreach($categories as $category)
+                            <tr>
+                                <td>
+                                    {{ $category->name }}
+                                </td>
+                                <td>
+                                    <a href="{{ route('category.edit', ['id' => $category->id]) }}" class="btn-xs btn-info">
+                                        Edit
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="{{ route('category.delete', ['id' => $category->id]) }}" class="btn-xs btn-danger">
+                                        Delete
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+
+
+                        @else
+
+                        <tr>
+                            <th colspan="5" class="text-center">No categories yet</th>
+                        </tr>
+
+                        @endif
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+
+
+@stop
---
+++ resources/views/admin/tags/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="card">
+
+        <div class="card-header">
+            Tags
+        </div>
+
+        <div class="card-body">
+
+            <table class="table table-hover">
+                <thead>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Edit
+                </th>
+                <th>
+                    Delete
+                </th>
+                </thead>
+
+                <tbody>
+                @if($tags->count()>0)
+                    @foreach($tags as $tag)
+                        <tr>
+                            <td>
+                                {{ $tag->tag }}
+                            </td>
+                            <td>
+                                <a href="{{ route('tag.edit', ['id' => $tag->id]) }}" class="btn-xs btn-info">
+                                    Edit
+                                </a>
+                            </td>
+                            <td>
+                                <a href="{{ route('tag.delete', ['id' => $tag->id]) }}" class="btn-xs btn-danger">
+                                    Delete
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+
+
+                @else
+
+                    <tr>
+                        <th colspan="5" class="text-center">No tags yet</th>
+                    </tr>
+
+                @endif
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+
+
+@stop
